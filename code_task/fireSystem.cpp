9314686d7be9c62690c04afa03b7f4fe837ccf70
Emily <100779155+Emili4@users.noreply.github.com>
2024-03-29 18:49:17 +0200

    Create fireSystem.cpp

---
+++ code_task/fireSystem.cpp
@@ -0,0 +1,19 @@
+class Drone {
+  FireScanner f1;
+};
+
+class FireScanner {
+private:
+  int fireLocation;
+public:
+  void setFireData(Satellite S1) {
+  fireLocation = S1.getGLData(); 
+  }
+};
+
+class Satellite {
+public:
+  int getGLData() { return geoLocationData;};
+private:
+  int geoLocationData;
+};
